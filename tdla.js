c71042ca3448d9b76002b57917cdbc9364e67964
priteshsingh-vats <42402753+priteshsingh-vats@users.noreply.github.com>
2019-03-09 23:22:27 +0530

    Add files via upload

---
+++ tdla.js
@@ -0,0 +1,18 @@
+$("ol").on("click","li",function(){
+$(this).css("color","grey");
+$(this).css("text-decoration","line-through");
+});
+$("ol").on("dblclick","li",function(){
+$(this).css("color","black");
+$(this).css("text-decoration","none");
+});
+$("ol").on("click","span",function(event){
+   $(this).parent().remove();
+    event.stopPropagation();
+});
+$("button").on("click",function()
+{
+  var todol = $("input").val();
+  $("ol").append('<li><span><i class="fas fa-trash-alt"></i></span>' + todol+"</li>");
+  $("input").val("");
+});
